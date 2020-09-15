17c0bd331f3aaffad48e97c3c485197b486665ee
Adit Damodaran <aditdamodaran@gmail.com>
2020-09-15 06:56:49 -0500

    adding back build folder

---
+++ client/build/precache-manifest.a3a516910d7716916d2d3fbb790f9a8a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b358b03fbd0381fce10f09875cbb8bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b10b33b4f7c633253fab",
+    "url": "/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "732eee81148f0a4a3d0e",
+    "url": "/static/css/main.f00b0ac4.chunk.css"
+  },
+  {
+    "revision": "b10b33b4f7c633253fab",
+    "url": "/static/js/2.99627cea.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.99627cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732eee81148f0a4a3d0e",
+    "url": "/static/js/main.8fdf10ff.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "641355259d0727533186c61484f0273b",
+    "url": "/static/media/list-icon.64135525.svg"
+  },
+  {
+    "revision": "93c1a16bb17baa502133a0c51bb6726b",
+    "url": "/static/media/pause-icon.93c1a16b.svg"
+  },
+  {
+    "revision": "4cb925101f252727155327863fcc40b5",
+    "url": "/static/media/play-icon-circular.4cb92510.svg"
+  },
+  {
+    "revision": "718f9d876e96f7a365c8722b3eef1629",
+    "url": "/static/media/play-icon.718f9d87.svg"
+  },
+  {
+    "revision": "0749b30b4128ba2c6b3c8a750e3cee4a",
+    "url": "/static/media/proxima_nova_bold.0749b30b.woff"
+  },
+  {
+    "revision": "53af5fac95bd9aa4a92f6922014dd17b",
+    "url": "/static/media/proxima_nova_bold.53af5fac.woff2"
+  },
+  {
+    "revision": "b10c6535f6089a8339837e622e8450af",
+    "url": "/static/media/proxima_nova_semibold.b10c6535.woff"
+  },
+  {
+    "revision": "d20d754a361445016f2483133da6f8d7",
+    "url": "/static/media/proxima_nova_semibold.d20d754a.woff2"
+  },
+  {
+    "revision": "9b57d5aa9e811cab48c2e748b09435ac",
+    "url": "/static/media/spotify-logo.9b57d5aa.svg"
+  }
+]);
